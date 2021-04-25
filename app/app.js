456ad58bf77455ed1552c2b24aee41b2b1e160ff
kovvdn <kovvdn@gmail.com>
2021-04-25 21:04:50 +0300

    add static midware

--- app/app.js
+++ app/app.js
@@ -1,17 +1,20 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 require("./middleware/global.middleware")(app);
 require("./routes/todo.routes")(app);
 
-app.get("/", (req, res) => {
-  res.send({ message: "ok" });
-});
-
 app.use((err, req, res, next) => {
   console.log(err);
   res.sendStatus(500);
+});
+
+console.log(process.cwd());
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(process.cwd(), "../todo-react/dist", "index.html"));
 });
 
 module.exports = app;
--- app/middleware/global.middleware.js
+++ app/middleware/global.middleware.js
@@ -1,9 +1,11 @@
 const logger = require("morgan");
 const express = require("express");
+const path = require("path");
 
 module.exports = (app) => {
   app
     .use(logger("dev"))
     .use(express.json())
-    .use(express.urlencoded({ extended: true }));
+    .use(express.urlencoded({ extended: true }))
+    .use(express.static(path.resolve(process.cwd(), "../todo-react/dist")));
 };
